370c5092b26c65794006943381910fbfe7aac7b6
ColtMort <balding.fenders.0e@icloud.com>
2023-05-31 15:00:05 -0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      scale: {
+        '10' : '0.1'
+      }
+    },
+  },
+  plugins: [],
+}
+
