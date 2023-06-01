e05a1763f19362810d23e40147854f77245148bb
ColtMort <balding.fenders.0e@icloud.com>
2023-06-01 00:59:51 -0600

    added contact info, minor layout adjustments

---
+++ src/server.js
@@ -0,0 +1,48 @@
+const cosmogeddonGithub = document.getElementById("cosmogeddon-github")
+
+console.log(cosmogeddonGithub)
+
+cosmogeddonGithub.addEventListener('click', (event) => {
+    event.stopPropagation()
+    window.open('https://github.com/coltmort/Cosmogeddon','_blank')
+
+})
+const semiSnitchGithub = document.getElementById("semi-snitch-github")
+
+console.log(semiSnitchGithub)
+
+semiSnitchGithub.addEventListener('click', (event) => {
+    event.stopPropagation()
+    window.open('https://github.com/greg4949/Semi-Snitch','_blank')
+
+})
+
+const hungryBeatsGithub = document.getElementById("hungry-beats-github")
+
+console.log(hungryBeatsGithub)
+
+hungryBeatsGithub.addEventListener('click', (event) => {
+    event.stopPropagation()
+    window.open('https://github.com/coltmort/Hungry-Beats','_blank')
+
+})
+
+const coltGithub = document.getElementById("colt-github")
+
+console.log(coltGithub)
+
+coltGithub.addEventListener('click', (event) => {
+    event.stopPropagation()
+    window.open('https://github.com/coltmort/','_blank')
+
+})
+
+const coltlinkedin = document.getElementById("colt-linkedin")
+
+console.log(coltlinkedin)
+
+coltlinkedin.addEventListener('click', (event) => {
+    event.stopPropagation()
+    window.open('https://www.linkedin.com/in/colton-mortensen/','_blank')
+
+})
